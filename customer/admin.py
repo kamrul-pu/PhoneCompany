2116615aadbe368e2bdeeb3ab00bf3cf1522b86c
Kamrul Hasan <kamrul.h456@gmail.com>
2023-02-03 01:31:47 +0600

    Models Added to admin panel

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from customer.models import *
 # Register your models here.
+
+
+admin.site.register(Company)
+admin.site.register(PhoneNumber)
+admin.site.register(Customer)
+admin.site.register(CustomerNumber)
+admin.site.register(Plan)
+admin.site.register(CustomerActivatedPlan)
