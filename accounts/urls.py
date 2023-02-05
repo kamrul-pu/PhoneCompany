c6270520cc0eec46061c54d4b9ed89764fa2b254
Kamrul Hasan <kamrul.h456@gmail.com>
2023-02-06 00:17:09 +0600

    Login Logout function added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',home,name="home"),
     path('login/',userLogin,name='login'),
+    path('logout/',userLogout,name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def home(request):
+    context = {}
+    return render(request,'accounts/home.html',context)
+
 def userLogin(request):
-    return render(request,'accounts/login.html')
+    form = AuthenticationForm()
+    context = {'form':form}
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            if user.is_active:
+                login(request,user)
+                return redirect("/")
+            else:
+                print("User is not active")
+    return render(request,'accounts/login.html',context)
+
+@login_required
+def userLogout(request):
+    logout(request)
+    return redirect("/")
